a56d1c4bfcbc2d14b8e2f0eaad9fbc7ae487a8c2
Yahya Muhammad <firzasaputra08@gmail.com>
2023-08-22 03:27:00 +0700

    update

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
-    for (let i = 1; i <= Infinity; i++) {
-        console.log("Error");
-    }
 });
+
 
 document.addEventListener("selectstart", function (e) {
     e.preventDefault();
